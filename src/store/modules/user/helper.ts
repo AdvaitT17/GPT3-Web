03f8bfef990ee7954671c71779617b0988398eb4
AdvaitT17 <advait1702@gmail.com>
2023-05-09 04:28:29 +0530

    Update helper.ts

--- src/store/modules/user/helper.ts
+++ src/store/modules/user/helper.ts
@@ -15,9 +15,9 @@ export interface UserState {
 export function defaultSetting(): UserState {
   return {
     userInfo: {
-      avatar: 'https://raw.githubusercontent.com/Chanzhaoyu/chatgpt-web/main/src/assets/avatar.jpg',
-      name: 'ChenZhaoYu',
-      description: 'Star on <a href="https://github.com/Chanzhaoyu/chatgpt-bot" class="text-blue-500" target="_blank" >GitHub</a>',
+      avatar: 'https://cdn.apexinfosys.in/web-core/advait-thakur.webp',
+      name: 'Advait Thakur',
+      description: 'Founder & CEO, Apex Infosys',
     },
   }
 }
